8e25bce478f1f6ff4be3c842b5341c87ac5cf149
Supadejh (Sun) PHATPANICHOT <sun-s.phatpanichot@student.bodwell.edu>
2018-01-20 12:29:13 -0800

    Initial commit

---
+++ src/Basics.java
@@ -0,0 +1,52 @@
+/**
+ * Execises to practice the basic syntax
+ */
+public class Basics {
+
+    public static void main(String[] args) {
+        // TODO: Write a Java program to print the sum of two numbers.
+        int a = 6;
+        int b = 15;
+        int sum;
+        sum = a+b;
+        System.out.println("the sum is " + sum);
+        // TODO: Write a Java program to divide two numbers and print on the screen. Use both integers and doubles.
+        int c = 42;
+        int d = 2;
+        int division;
+        division = c/d;
+        System.out.println("the quotient is "+division);
+
+        // TODO: Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+        int s = 21;
+        int t = 10;
+        int addition;
+        int product;
+        int subtract;
+        double divide;
+        addition = s+t;
+        product = s*t;
+        subtract = s-t;
+        divide = s/t;
+        System.out.println("the addition is "+addition);
+        System.out.println("the product is "+product);
+        System.out.println("the subtraction is "+subtract);
+        System.out.println("the division is "+divide);
+
+
+
+        // TODO: Write a Java program to swap two variables. Print the before and after switching.
+    }
+    int uu, ii;
+    uu = 21;
+    ii = 0;
+    int ff = uu;
+    uu = ii;
+    ii = ff;
+    System.out.println("/n")
+
+
+
+
+
+
